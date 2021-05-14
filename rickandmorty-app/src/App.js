5cb2a04725b021ea8dada91337301829cd47e6bf
Florian Schmidbauer <f.schmidbauer@me.com>
2021-05-14 13:00:03 +0200

    presets part2 made

---
+++ rickandmorty-app/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { useState } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+export default function App() {
+  return <div></div>;
+}
---
+++ rickandmorty-app/src/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    line-height: 1.5;
+    font-family: verdana;
+}
+`;
